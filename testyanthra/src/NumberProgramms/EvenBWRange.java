3dcb5a744c4b450c28ba5c10fe4dfc8fa2810aee
ramir <ramir@Thriveni>
2023-04-04 13:02:46 +0530

    new

---
+++ testyanthra/src/NumberProgramms/EvenBWRange.java
@@ -0,0 +1,17 @@
+package NumberProgramms;
+
+public class EvenBWRange {
+
+	public static void main(String[] args) {
+		int start=30;
+		int  end=40;
+		for(int i=start;i<=end;i++){
+		if(i%2==0){
+		System.out.println("Even number are :" +i);
+		}
+		}
+
+
+	}
+
+}
