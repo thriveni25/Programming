df74096043ea25c04cf40f4cd59e5687fe754dfc
ramir <ramir@Thriveni>
2023-04-04 13:01:38 +0530

    New codes

--- testyanthra/src/NumberProgramms/DuckNum.java
+++ testyanthra/src/NumberProgramms/DuckNum.java
@@ -3,7 +3,7 @@ package NumberProgramms;
 public class DuckNum {
 
 	public static void main(String[] args) {
-		int n=0702;
+		/*int n=702;
 		int c=0;
 		boolean flag=false;
 		while(n>9) {
@@ -22,9 +22,35 @@ public class DuckNum {
 		}
 		}
 		if(flag==true) System.out.println("Duck num");
-		else System.out.println("Not Duck");
+		else System.out.println("Not Duck");*/
 		
+		
+		String s="325013";
+		
+		boolean flag=true;
+		if(s.charAt(0)=='0') 
+		{
+			flag=false;
 
+		}
+		else 
+		{
+		for(int i=1;i<s.length();i++) {
+			
+			if(s.charAt(i)=='0')
+			{
+				flag=true;
+				break;
+			}
+			else
+			flag=false;
+		}
 	}
-
+		if(flag==true) {
+			System.out.println("duck number");
+		}
+		else {
+			System.out.println("not duck");
+		}
+	}
 }
--- testyanthra/src/NumberProgramms/Fibonacci.java
+++ testyanthra/src/NumberProgramms/Fibonacci.java
@@ -8,12 +8,12 @@ public class Fibonacci {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter num: ");
 		int n=sc.nextInt();
-		int a=0,b=1,c;
+		int a=0,b=1,c=0;
 		for(int i=1;i<=n;i++) {
 			
-			//if(i==n) {
+			if(i==n) {
 			System.out.print(a+" ");
-			//}
+			}
 			c=a+b;
 			a=b;
 			b=c;
